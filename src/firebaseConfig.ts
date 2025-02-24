7da09eb79cc97bf46daf7e4fc733f5f56b4c3518
mkj-kento-horikami <kento.horikami@weblab.t.u-tokyo.ac.jp>
2025-02-24 14:21:15 +0900

    Move Firebase config to .env file

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -5,14 +5,14 @@ import { getAuth } from "firebase/auth";
 
 // Firebase設定
 const firebaseConfig = {
-  apiKey: "AIzaSyB4A6zSs_kP78em2ywaqWFzUAqe6lqJhrM",
-  authDomain: "lms-inc-b0bc1.firebaseapp.com",
-  projectId: "lms-inc-b0bc1",
-  storageBucket: "lms-inc-b0bc1.firebasestorage.app",
-  messagingSenderId: "18093679559",
-  appId: "1:18093679559:web:4de22c8fd4e3aca79c6fe2",
-  measurementId: "G-VWFNTQLT7P"
-};
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  };
 
 // Firebase初期化
 const app = initializeApp(firebaseConfig);
