98a54b5c05e4d4b5852c04d661a594d8f6c310d6
mkj-kento-horikami <kento.horikami@weblab.t.u-tokyo.ac.jp>
2025-03-01 11:19:01 +0900

    Update Header.tsx to change background color for admin and add hover effect~

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
